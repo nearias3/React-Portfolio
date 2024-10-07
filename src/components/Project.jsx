8f775552393855828ddc34ad44f8f4a8098b375b
nearias3 <127260408+nearias3@users.noreply.github.com>
2024-10-07 15:57:32 -0500

    Added code for project.jsx

---
+++ src/components/Project.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Project = ({ title }) => {
+  return (
+    <div>
+      <h3>{title}</h3>
+      {/* Here I'll add all the specific project details */}
+    </div>
+  );
+};
+
+export default Project;
