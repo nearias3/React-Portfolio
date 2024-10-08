baaab82215660706c462c80739b18ff29d9c41d1
nearias3 <127260408+nearias3@users.noreply.github.com>
2024-10-08 14:08:28 -0500

    Added more project components like description, links, and images

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,16 +1,38 @@
 import PropTypes from "prop-types";
+import '../styles/Project.css';
 
-const Project = ({ title }) => {
+const Project = ({ title, description, image, liveLink, repoLink }) => {
   return (
-    <div>
+    <div className="project">
+      <img
+        src={image}
+        alt={`Screenshot of ${title}`}
+        className="project-image"
+      />
       <h3>{title}</h3>
-      {/* Here I'll add all the specific project details */}
+      <p>{description}</p>
+      <div className="project-links">
+        {liveLink && (
+          <a href={liveLink} target="_blank" rel="noopener noreferrer">
+            View Live
+          </a>
+        )}
+        {repoLink && (
+          <a href={repoLink} target="_blank" rel="noopener noreferrer">
+            GitHub Repository
+          </a>
+        )}
     </div>
+  </div>
   );
 };
 
 Project.propTypes = {
   title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  liveLink: PropTypes.string,
+  repoLink: PropTypes.string,
 };
 
 export default Project;
