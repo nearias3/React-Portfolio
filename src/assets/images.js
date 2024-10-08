b6d5a7e3c39fcfb44cc774ddf49bf11913149486
nearias3 <127260408+nearias3@users.noreply.github.com>
2024-10-08 17:50:51 -0500

    Added all of the projects to Portfolio.jsx, added images folder to assets and exported that to image.js so that I could easily import the images to my Portfolio page

---
+++ src/assets/images.js
@@ -0,0 +1,35 @@
+import challenge01 from "./images/challenge01.png";
+import challenge03 from "./images/challenge03.png";
+import challenge05 from "./images/challenge05.png";
+import challenge06 from "./images/challenge06.png";
+import project1 from "./images/project1.png";
+import challenge09 from "./images/challenge09.png";
+import challenge10 from "./images/challenge10.png";
+import challenge11 from "./images/challenge11.png";
+import challenge12 from "./images/challenge12.png";
+import challenge13 from "./images/challenge13.png";
+import challenge14 from "./images/challenge14.png";
+import project2 from "./images/project2.png";
+import challenge17 from "./images/challenge17.png";
+import challenge18 from "./images/challenge18.png";
+import challenge19 from "./images/challenge19.png";
+
+const images = {
+  challenge01,
+  challenge03,
+  challenge05,
+  challenge06,
+  project1,
+  challenge09,
+  challenge10,
+  challenge11,
+  challenge12,
+  challenge13,
+  challenge14,
+  project2,
+  challenge17,
+  challenge18,
+  challenge19,
+};
+
+export default images;
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,185 +1,123 @@
 import Project from "../components/Project";
 import "../styles/Portfolio.css";
+import images from "../assets/images";
 
 const projects = [
   {
     title: "Code Refactor",
     description: "This app is a refactor of a website. I cleaned up the code, fixed a couple of issues for the client, and made it more accessible.",
-    image: "assets/images/challenge01.png",
+    image: images.challenge01,
     liveLink: "https://nearias3.github.io/Challenge-01/",
     repoLink: "https://github.com/nearias3/Challenge-01"
   },
   {
     title: "Payroll Tracker",
     description: "This app is a tracker that helps employers keep track of payroll, with additional information stored in the console.",
-    image: "assets/images/challenge03.png",
+    image: images.challenge03,
     liveLink: "https://nearias3.github.io/Employee-Payroll-Tracker/",
     repoLink: "https://github.com/nearias3/Employee-Payroll-Tracker"
   },
 
   {
-    title: "",
-    description: "",
-    image: "",
-    liveLink: "",
-    repoLink: ""
-  }
+    title: "Task Board",
+    description: "This app allows the user to input information into a modal and create task cards that include due dates and descriptions.",
+    image: images.challenge05,
+    liveLink: "https://nearias3.github.io/Task-Board/",
+    repoLink: "https://github.com/nearias3/Task-Board/"
+  },
 
   {
-    title: "",
-    description: "",
-    image: "",
-    liveLink: "",
-    repoLink: ""
-  }
+    title: "Weather Dashboard",
+    description: "This app allows users to search for the current weather in a city and view dynamically updated weather forecasts.",
+    image: images.challenge06,
+    liveLink: "https://nearias3.github.io/Weather-Dashboard/",
+    repoLink: "https://github.com/nearias3/Weather-Dashboard/"
+  },
 
   {
-    title: "",
-    description: "",
-    image: "",
-    liveLink: "",
-    repoLink: ""
-  }
+    title: "gigON!",
+    description: "In this collaborative project, the user can login using their Spotify account, view their top artists, and  search for concerts by those artists using their location.",
+    image: images.project1,
+    liveLink: "https://cneale92.github.io/gigON/",
+    repoLink: "https://github.com/cneale92/gigON/"
+  },
 
   {
-    title: "",
-    description: "",
-    image: "",
-    liveLink: "",
-    repoLink: ""
-  }
+    title: "README Generator",
+    description: "In this app, the user can automate the creation of README files using a node powered prompt in the terminal.",
+    image: images.challenge09,
+    repoLink: "https://github.com/nearias3/README-Generator"
+  },
 
   {
-    title: "",
-    description: "",
-    image: "",
-    liveLink: "",
-    repoLink: ""
-  }
+    title: "Logo Maker",
+    description: "This app allows the user to automate the creation of an SVG logo. In the terminal, the user is prompted to choose their color, shape, and text.",
+    image: images.challenge10,
+    repoLink: "https://github.com/nearias3/SVG-Logo-Maker"
+  },
 
   {
-    title: "",
-    description: "",
-    image: "",
-    liveLink: "",
-    repoLink: ""
-  }
+    title: "Note Taker",
+    description: "This app provides users with space to take notes. They can input their notes, save them on their browser, and delete them as needed.",
+    image: images.challenge11,
+    liveLink: "https://note-taker-kohj.onrender.com",
+    repoLink: "https://github.com/nearias3/Miniature-Eureka"
+  },
 
   {
-    title: "",
-    description: "",
-    image: "",
-    liveLink: "",
-    repoLink: ""
-  }
+    title: "SQL Employee Tracker",
+    description: "This command-line application allows users to manage a company's employee database using node, inquirer, and postgreSQL.",
+    image: images.challenge12,
+    repoLink: "https://github.com/nearias3/SQL-Employee-Tracker"
+  },
 
 
   {
-    title: "",
-    description: "",
-    image: "",
-    liveLink: "",
-    repoLink: ""
-  }
+    title: "E-Commerce Backend",
+    description: "This app is the back-end for an e-commerce site, built using Express, Sequelize, and PostgreSQL.",
+    image: images.challenge13,
+    repoLink: "https://github.com/nearias3/ecommerce-backend"
+  },
 
 
   {
-    title: "",
-    description: "",
-    image: "",
-    liveLink: "",
-    repoLink: ""
-  }
+    title: "Tech Blog",
+    description: "This app is a full-stack site that allows users to create their own accounts, publish blogs, and comment on other users' posts.",
+    image: images.challenge14,
+    liveLink: "https://tech-blog-6wke.onrender.com/",
+    repoLink: "https://github.com/nearias3/Tech-Blog/"
+  },
 
   {
-    title: "",
-    description: "",
-    image: "",
-    liveLink: "",
-    repoLink: ""
-  }
+    title: "BookNest",
+    description: "In this collaborative project, we bring you BookNest, an app that facilitate's peer-to-peer book lending! Users can create their own collection of books, share with others, and borrow from within their communities.",
+    image: images.project2,
+    liveLink: "https://p2-book-nest.onrender.com/",
+    repoLink: "https://github.com/Lixiviate/P2-Book-Nest"
+  },
 
   {
-    title: "",
-    description: "",
-    image: "",
-    liveLink: "",
-    repoLink: ""
-  }
-
-
-  {
-    title: "",
-    description: "",
-    image: "",
-    liveLink: "",
-    repoLink: ""
-  }
+    title: "Regex Tutorial",
+    description: "This is a gist tutorial in which I breakdown the Regex generator.",
+    image: images.challenge17,
+    liveLink: "https://gist.github.com/nearias3/b460fd7e73e3f0c807bf5b5856d3006e",
+  },
 
 
   {
-    title: "",
-    description: "",
-    image: "",
-    liveLink: "",
-    repoLink: ""
-  }
+    title: "Social Network API",
+    description: "This app is the backend for a social networking site built with Node, Express, MongoDB and Mongoose.",
+    image: images.challenge18,
+    repoLink: "https://github.com/nearias3/Social-Network-API"
+  },
 
 
   {
-    title: "",
-    description: "",
-    image: "",
-    liveLink: "",
-    repoLink: ""
-  }
-
-  {
-    title: "",
-    description: "",
-    image: "",
-    liveLink: "",
-    repoLink: ""
-  }
-
-  {
-    title: "",
-    description: "",
-    image: "",
-    liveLink: "",
-    repoLink: ""
-  }
-
-  {
-    title: "",
-    description: "",
-    image: "",
-    liveLink: "",
-    repoLink: ""
-  }
-
-  {
-    title: "",
-    description: "",
-    image: "",
-    liveLink: "",
-    repoLink: ""
-  }
-
-  {
-    title: "",
-    description: "",
-    image: "",
-    liveLink: "",
-    repoLink: ""
-  }
-
-  {
-    title: "",
-    description: "",
-    image: "",
-    liveLink: "",
-    repoLink: ""
+    title: "Text Editor",
+    description: "This app is a PWA text editor that runs on the browser, works both online and offline, and stores data using IndexedDB.",
+    image: images.challenge19,
+    liveLink: "https://text-editor-mzng.onrender.com",
+    repoLink: "https://github.com/nearias3/Text-Editor"
   }
 
 ];
